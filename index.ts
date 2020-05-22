2ef440414e7dd2188740c257d0a67d531ea2b55d
mrjasonweaver <indyplanets@gmail.com>
2020-05-22 16:23:36 -0500

    timing and translate options

--- index.ts
+++ index.ts
@@ -3,46 +3,56 @@ const topLeft = document.querySelector("#triangle-top-left");
 const topRight = document.querySelector("#triangle-top-right");
 const bottom = document.querySelector("#triangle-bottom");
 
-logo.animate([
-  // keyframes
-  { transform: 'translateY(0px)' }, 
-  { transform: 'translateY(-5px)' },
-  { transform: 'translateY(0px)' }
-], { 
-  // timing options
-  duration: 300,
-  iterations: Infinity
-});
+interface TimingOptions {
+  duration: number,
+  iterations: number
+}
 
-topLeft.animate([
-  // keyframes
-  { transform: 'translate(0px)' }, 
-  { transform: 'translate(-5px, -5px)' },
-  { transform: 'translate(0px)' }
-], { 
-  // timing options
-  duration: 1000,
-  iterations: Infinity
-});
+const durationSlow = 3000;
+const durationMedium = 1000;
+const durationFast = 500;
 
-topRight.animate([
-  // keyframes
-  { transform: 'translate(0px)' }, 
-  { transform: 'translate(5px, -5px)' },
-  { transform: 'translate(0px)' }
-], { 
-  // timing options
-  duration: 1000,
-  iterations: Infinity
-});
+const timingOptions = (duration: number, iterations: number): TimingOptions => {
+  return { duration, iterations }
+}
 
-bottom.animate([
-  // keyframes
-  { transform: 'translate(0px)' }, 
-  { transform: 'translate(0, 5px)' },
-  { transform: 'translate(0px)' }
-], { 
-  // timing options
-  duration: 1000,
-  iterations: Infinity
-});
+const translateOptions = (args: Array<string>[]): Keyframe[] => {
+  return args.map(item => {
+    return {
+      transform: `translate(${item[0]}, ${item[1]})`
+    }
+  });
+}
+
+const breakOutAnimationTopLeft = [
+  ['0px', '0px'],
+  ['-3px', '-3px'],
+  ['0px', '0px']
+];
+
+const breakOutAnimationTopRight = [
+  ['0px', '0px'],
+  ['3px', '-3px'],
+  ['0px', '0px']
+];
+
+const breakOutAnimationBottom = [
+  ['0px', '0px'],
+  ['0', '3px'],
+  ['0px', '0px']
+];
+
+topLeft.animate(
+  translateOptions(breakOutAnimationTopLeft),
+  timingOptions(durationMedium, Infinity)
+);
+
+topRight.animate(
+  translateOptions(breakOutAnimationTopRight),
+  timingOptions(durationMedium, Infinity)
+);
+
+bottom.animate(
+  translateOptions(breakOutAnimationBottom),
+  timingOptions(durationMedium, Infinity)
+);
