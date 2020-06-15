12b97ac61c8d50b824c40a4bde81016a811fee16
mrjasonweaver <indyplanets@gmail.com>
2020-06-15 14:54:37 -0500

    styling

--- index.ts
+++ index.ts
@@ -8,10 +8,12 @@ const username = document.querySelector('#username');
 const userForm = document.querySelector('#getUserForm');
 const userContainer = document.querySelector('#github-user');
 const usersContainer = document.querySelector('#github-users');
-
+const title = document.querySelector('#title');
+const perPage = '100';
 const durationSlow = 1200;
 const durationMedium = 800;
 const durationFast = 500;
+let city: string;
 
 interface TimingOptions {
   duration: number,
@@ -41,6 +43,7 @@ userForm.addEventListener('submit', event => {
   event.preventDefault();
   userContainer.innerHTML = '';
   usersContainer.innerHTML = '';
+  title.innerHTML = 'Searching GitHub...';
   animateLogo(true);
   const userUrl = `${fetchUserInfoURL}${event.target[0].value}`;
   fetchUserInfo(userUrl);
@@ -49,14 +52,16 @@ userForm.addEventListener('submit', event => {
 const fetchUserInfo = async (url: RequestInfo): Promise<void> => {
   let response = await fetch(url);
   let data = await response.json();
-  const location = data.location.split(',')[0];
-  const searchUrl = `${searchUsersURL}?q=+location:${location}+type:user+-user:${data.login}`; 
+  city = data.location.split(',')[0];
+  const searchUrl = `${searchUsersURL}?q=+location:${city}+type:user+-user:${data.login}&page=1&per_page=${perPage}`; 
   addGithubUser(data, searchUrl);
 };
 
 const searchUsers = async (url: RequestInfo): Promise<void> => {
   let response = await fetch(url);
   let data = await response.json();
+  const infoObj = {total: data.total_count, count: perPage, city};
+  title.innerHTML = titleContent(infoObj);
   addSearchedGithubUsers(data.items);
 };
 
@@ -73,6 +78,11 @@ const addSearchedGithubUsers = (content: GitHubUser[]): void => {
   });
   animateLogo(false);
 };
+
+const titleContent = (info): string => {
+  const { total, count, city } = info;
+  return `Showing ${count} of ${total} GitHub users in ${city}`;
+}
 
 const timingOptions = (duration: number, iterations: number): TimingOptions => {
   return { duration, iterations }
@@ -101,14 +111,15 @@ const breakOutAnimationBottom: Array<string>[] = [
 ];
 
 const fillAnimation: Array<string>[] = [
-  ['rgba(255,255,255,1)'],
-  ['rgba(223, 78, 113, .8)'],
-  ['rgba(255,255,255,1)']
+  ['rgba(0,0,0,1)'],
+  ['rgba(200, 100, 100, .7)'],
+  ['rgba(170, 255, 150, .7)'],
+  ['rgba(0,0,0,1)']
 ];
 
 const animateLogo = (playing: boolean): void => {
   const logoAnimation = logo.animate(
-   fillOptions(fillAnimation),
+    fillOptions(fillAnimation),
     timingOptions(durationMedium, 5)
   );
   return playing ? logoAnimation.play() : logoAnimation.pause()
